0d1e79c5101b98d73c1573153f19aaa37dab5211
atachimiko <atachi.miko@gmail.com>
2018-10-02 00:49:30 +0900

    サムネイルのダミー画像を追加

--- Controllers/ThumbnailController.cs
+++ Controllers/ThumbnailController.cs
@@ -1,10 +1,10 @@
 using System;
 using System.Linq;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Net.Http.Headers;
 using Foxpict.Service.Infra.Model;
 using Foxpict.Service.Infra.Repository;
 using Foxpict.Service.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
 
 namespace Foxpict.Service.Web.Controllers {
   /// <summary>
@@ -31,16 +31,26 @@ namespace Foxpict.Service.Web.Controllers {
     public IActionResult FetchThumbnail (string thumbnailKey) {
       long thumbnailId = 0L;
       IThumbnail thumbnail = null;
-      if (long.TryParse (thumbnailKey, out thumbnailId)) {
-        thumbnail = thumbnailRepository.Load (thumbnailId);
-      } else {
-        thumbnail = thumbnailRepository.FindByKey (thumbnailKey).FirstOrDefault ();
+      try {
+        if (long.TryParse (thumbnailKey, out thumbnailId)) {
+          thumbnail = thumbnailRepository.Load (thumbnailId);
+        } else {
+          thumbnail = thumbnailRepository.FindByKey (thumbnailKey).FirstOrDefault ();
+        }
+      } catch (Exception) {
+        // サムネイルが正常に取得できない場合は、常にnullとする。
+        thumbnail = null;
       }
-      if (thumbnail == null) throw new ApplicationException (string.Format ("サムネイル画像({0})が見つかりません", thumbnailKey));
 
       // リソースの有効期限等を決定する
       //DateTimeOffset now = DateTime.Now;
       //var etag = new EntityTagHeaderValue("\"" + Guid.NewGuid().ToString() + "\"");
+
+      if (thumbnail == null) {
+        System.Reflection.Assembly assm = System.Reflection.Assembly.GetExecutingAssembly ();
+        string filePath = string.Format ("Foxpict.Service.Web.Assets.Icon.UI.Article3.png");
+        return new FileStreamResult (assm.GetManifestResourceStream (filePath), "image/png");
+      }
 
       return new FileContentResult (thumbnail.BitmapBytes, thumbnail.MimeType);
     }
